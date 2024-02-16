b1a64d94ab3c0f8127ce1a13674b2689e1a64be8
ABHILASH <99235816+M4X1M4S@users.noreply.github.com>
2024-02-17 01:19:18 +0530

    Create p3.js

---
+++ p3.js
@@ -0,0 +1,29 @@
+/*
+Write a function that calculates the time (in seconds) it takes for the JS code to calculate sum from 1 to n, given n as the input.
+Try running it for
+1. Sum from 1-100
+2. Sum from 1-100000
+3. Sum from 1-1000000000
+Hint - use Date class exposed in JS
+*/
+
+
+
+
+function sum(n){
+    let sum=0;
+    for(let i=0;i<=n;i++){
+        sum+=i;
+    }
+}
+function calculateTime(n){
+    const start= new Date().getTime();
+      sum(n);
+    const end=new Date().getTime();
+    let timetaken=(end-start)/1000;
+    return(timetaken)
+}
+
+console.log(calculateTime(100)+"sec")
+console.log(calculateTime(100000)+"sec")
+console.log(calculateTime(1000000000)+"sec")
